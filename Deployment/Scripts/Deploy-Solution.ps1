d5a9c7dde88d170efc46cfd77c82fc31d69ef66a
Luise Freese <luise@raeuberleiterin.de>
2021-11-26 15:30:35 +0100

    adds TeamMember.ReadWrite.All permissions to managed identity

--- Deployment/Scripts/Deploy-Solution.ps1
+++ Deployment/Scripts/Deploy-Solution.ps1
@@ -160,7 +160,8 @@ $directoryReadWriteAll = az ad sp show --id $graphId --query "appRoles[?value=='
 $groupCreate = az ad sp show --id $graphId --query "appRoles[?value=='Group.Create'].id | [0]" -o tsv
 $sitesManageAll = az ad sp show --id $graphId --query "appRoles[?value=='Sites.Manage.All'].id | [0]" -o tsv
 $sitesReadWriteAll = az ad sp show --id $graphId --query "appRoles[?value=='Sites.ReadWrite.All'].id | [0]" -o tsv
-$appRoleIds = $teamCreate, $readWriteAll, $directoryReadWriteAll, $groupCreate, $sitesManageAll, $sitesReadWriteAll
+$teamMemberReadWriteAll = az ad sp show --id $graphId --query "appRoles[?value=='TeamMeber.ReadWite.All'].id | [0]" -o tsv 
+$appRoleIds = $teamCreate, $readWriteAll, $directoryReadWriteAll, $groupCreate, $sitesManageAll, $sitesReadWriteAll, $teamMemberReadWriteAll
 #Loop over all appRoleIds
 foreach ($appRoleId in $appRoleIds) {
     $roleMatch = $currentRoles -match $appRoleId
